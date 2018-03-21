830b523e6c501c46fa34c1fc66048d16ecf64259
klihelp <klihelp@users.noreply.github.com>
2018-03-21 11:07:09 +0000

    Update front_functions.php

--- front_functions.php
+++ front_functions.php
@@ -13,23 +13,26 @@
     * @return string
     *
     */
-    if ( !is_admin() ) {
+    if ( ! is_admin() ) {
         add_filter('the_content', function($content) {
             if ( ! is_main_query() ) {
                 return $content;
             }
-
             // Find gallery blocks
             $regexp = "<ul\s[^>]*wp-block-gallery[^>]*>(.*)<\/ul>";
-            if (preg_match_all("/$regexp/imsU", $content, $matches, PREG_PATTERN_ORDER)) {
+            if ( preg_match_all("/$regexp/imsU", $content, $matches, PREG_PATTERN_ORDER) ) {
                 $updated = false;
                 foreach($matches[0] as $key => $match) {
                     // Find images ids
-                    if ( preg_match_all('/$\sdata-id="(.*)"/imsU', $match, $match_ids, PREG_PATTERN_ORDER) ) {
-                        $matches[1][$key] = do_shortcode( '[gallery ids="'. implode(',', $match_ids[1]) .'"]' );
+                    if ( preg_match_all('/\sdata-id="(.*)"/imsU', $match, $match_ids, PREG_PATTERN_ORDER) ) {
+                        /**
+                         * We are still before do_shortcode , which has filter priority 10
+                         */
+                        $matches[1][$key] = '[gallery ids="'. implode(',', $match_ids[1]) .'"]';
                         $updated = true;
                     }
                 }
+                var_dump($updated);
                 if ($updated) {
                     $content = str_replace($matches[0], $matches[1], $content);
                 }
@@ -37,3 +40,4 @@
             return $content;
         }, 9);  
     }
+
